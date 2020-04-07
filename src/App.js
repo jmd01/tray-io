f097f0927b54b6189defa60bbf7c7428cb8db8df
john davison <john01davison@gmail.com>
2020-04-07 10:27:23 +0100

    Removed unneeded default CRA setup and added basic Visualiser component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import EventEmitter from '@trayio/builder-squad-event-emitter'
+
+import Visualiser from './components/Visualiser'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <EventEmitter>
+        <Visualiser />
+      </EventEmitter>
   );
 }
 
---
+++ src/components/Visualiser/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Visualiser(props) {
+    console.log(props)
+    return (
+        <h1>Visualiser</h1>
+    )
+}
+export default Visualiser
