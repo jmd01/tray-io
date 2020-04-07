7487788eb4d7a8d90721fb137493e3fdacc9ca17
john davison <john01davison@gmail.com>
2020-04-07 11:35:27 +0100

    Added comments and updated readme

--- src/components/Visualiser/index.js
+++ src/components/Visualiser/index.js
@@ -14,14 +14,20 @@ const initItem = {
 }
 
 function Visualiser(props) {
+    // Event items array populated from the "data-tray" prop
     const [dataTray, setDataTray] = useState([])
+
+    // useEffect requires a simple variable to be used in the dependency list
     const dataTrayProp = props["data-tray"]
 
     useEffect(() => {
+
+        // Ensure each item has a valid name, url and coords object structure
         const formatItem = (item) => {
             let xCoord
             let yCoord
 
+            // Consider importing lodash to reduce this by a few lines
             if (item.hasOwnProperty('coords') && (typeof item.coords === 'object')) {
                 xCoord = item.coords.hasOwnProperty('x') ?
                     Math.abs(item.coords.x) : initItem.coords.x
@@ -43,12 +49,9 @@ function Visualiser(props) {
             }
         }
 
-
-        const formattedItem = formatItem(dataTrayProp)
-
         setDataTray(items => [
             ...items,
-            formattedItem
+            formatItem(dataTrayProp)
         ])
 
     }, [dataTrayProp])
